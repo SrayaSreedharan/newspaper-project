58c95a21a9c0424aa72b5f0db0eca91b1cb26b7e
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-07 16:35:11 +0530

    testing purpose

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbarh = () => {
       <Navbar expand="lg" className="bg-body-tertiary row" >
       <Container className='col-md'>
         <Navbar.Brand href="/" style={{textDecoration:'none',fontSize:'36px'}} >NEWS</Navbar.Brand>
-            <Nav.Link href="/login" style={{textDecoration:'none',color:'white',padding:'10px',marginLeft:'1100px',fontSize:'30px'}} className='nav'>Login</Nav.Link>
+            <Nav.Link href="/login" style={{textDecoration:'none',color:'white',padding:'10px',fontSize:'30px'}} className='nav'>Login</Nav.Link>
             <Nav.Link href="/signup" style={{textDecoration:'none',color:'white',padding:'10px',fontSize:'30px'}} className='nav'>Sign up</Nav.Link>
       </Container>
     </Navbar>
--- src/Components/Navbars.jsx
+++ src/Components/Navbars.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbars.css'
-const Navbars =()=> {
+import './Navbars.css';
+
+const Navbars = () => {
   return (
-    <>
     <div className='nav'>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className=''>
-        <Navbar.Brand href="/" style={{textDecoration:'none',fontSize:'36px'}}>NEWS</Navbar.Brand>
-            <Nav.Link href="/general" style={{textDecoration:'none'}} className='nav'>General</Nav.Link>
-            <Nav.Link href="/world" style={{textDecoration:'none'}} className='nav'>World</Nav.Link>
-            <Nav.Link href="/nation" style={{textDecoration:'none'}} className='nav'>Nation</Nav.Link>
-            <Nav.Link href="/buisness" style={{textDecoration:'none'}} className='nav'>Business</Nav.Link>
-            <Nav.Link href="/technology" style={{textDecoration:'none'}} className='nav'>Technology</Nav.Link>
-            <Nav.Link href="/entertainment" style={{textDecoration:'none'}} className='nav'>Entertainment</Nav.Link>
-            <Nav.Link href="/sports" style={{textDecoration:'none'}} className='nav'>Sports</Nav.Link>
-            <Nav.Link href="/science" style={{textDecoration:'none'}} className='nav'>Science</Nav.Link>
-            <Nav.Link href="/health" style={{textDecoration:'none'}} className='nav'>Health</Nav.Link>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" bg="dark" variant="dark" className="px-3">
+        <Container fluid>
+          <Navbar.Brand href="/" style={{ textDecoration: 'none', fontSize: '36px' }}>
+            NEWS
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+              <Nav.Link href="/general" className="nav-link-custom">General</Nav.Link>
+              <Nav.Link href="/world" className="nav-link-custom">World</Nav.Link>
+              <Nav.Link href="/nation" className="nav-link-custom">Nation</Nav.Link>
+              <Nav.Link href="/buisness" className="nav-link-custom">Business</Nav.Link>
+              <Nav.Link href="/technology" className="nav-link-custom">Technology</Nav.Link>
+              <Nav.Link href="/entertainment" className="nav-link-custom">Entertainment</Nav.Link>
+              <Nav.Link href="/sports" className="nav-link-custom">Sports</Nav.Link>
+              <Nav.Link href="/science" className="nav-link-custom">Science</Nav.Link>
+              <Nav.Link href="/health" className="nav-link-custom">Health</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-    </>
-  )  
-}
-export default Navbars
+  );
+};
+
+export default Navbars;
